a4542882591dbaaad7b1df7ae3e58ec3a80a1764
Jacky Tran <tdhungit@gmail.com>
2022-03-03 15:18:38 +0700

    fix bugs

---
+++ saas_trial/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'Jinchatsh SAAS',
+	'author': 'Youaddon',
+	'version': '15.0.0.0',
+	'live_test_url': ' ',
+	'summary': 'Jinchatsh SAAS',
+	'description': """Jinchatsh SAAS""",
+	'license': "OPL-1",
+	'depends': ['base'],
+	'data': [
+
+	],
+	'assets': {
+		'web.assets_backend': [
+			'saas_trial/static/css/demo.css',
+		],
+	},
+	'installable': True,
+	'auto_install': False,
+	'category': 'Customizations',
+}
